884f5c3c875e5b4e55a85a130642c9c1375974df
mark8128 <markmahony@Marks-MacBook-Pro.local>
2015-03-26 12:51:07 -0400

    Added back button

--- KTCS_main.php
+++ KTCS_main.php
@@ -34,7 +34,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">KTCS</a>
+          <a class="navbar-brand" href="KTCS_main.php">KTCS</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <form class="navbar-form navbar-right" action="KTCS_login.php">
