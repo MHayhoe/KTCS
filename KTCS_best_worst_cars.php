4ed30820762354034716dcd486c5a61b29ac85eb
MHayhoe <m.hayhoe@icloud.com>
2015-03-27 15:01:02 -0400

    fixed best/worst

--- KTCS_best_worst_cars.php
+++ KTCS_best_worst_cars.php
@@ -21,13 +21,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-<<<<<<< HEAD
-  
-  <style>
-=======
     
     <style>
->>>>>>> FETCH_HEAD
 		#def {
 			width: 100%;
 			border-collapse: collapse;
@@ -111,11 +106,7 @@
 
 		 $result = mysqli_query($cxn, $query);
 		 
-<<<<<<< HEAD
-		echo '<table cellpadding="5" cellspacing="5" class="db-table" id="def" border="1">';
-=======
 		echo '<table cellpadding="5" cellspacing="5" class="db-table" id = "def" border="1">';
->>>>>>> FETCH_HEAD
 		$column = $result->fetch_fields();
 
 		echo '<tr>';
